f2565c64096476b86579ab3cb49f118a12c1a195
shi514652697 <ssnegi_4all@yahoo.com>
2020-05-18 19:19:41 -0500

    Added for new change

---
+++ SpringRestWebService/src/main/java/com/spring/in28minutes/SpringRestWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.spring.in28minutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestWebServiceApplication.class, args);
+	}
+
+}
---
+++ SpringRestWebService/src/test/java/com/spring/in28minutes/SpringRestWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.in28minutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
